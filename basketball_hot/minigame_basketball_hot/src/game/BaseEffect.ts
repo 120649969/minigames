c43bbcdcbad1a4ee0bd6be22c40a8c96d43d23eb
zhaowumi <wuqiang@badambiz.com>
2018-11-10 16:18:48 +0800

    删除默认的资源

---
+++ basketball_hot/minigame_basketball_hot/src/game/BaseEffect.ts
@@ -0,0 +1,14 @@
+class BaseEffect extends egret.DisplayObjectContainer{
+	public constructor(playerBall:PlayerBall) {
+		super()
+	}
+
+	public Update(need_create:boolean):void
+	{
+
+	}
+
+	public SetGoal(score:number):void
+	{
+	}
+}
