2bcbb3d45c4d1a8c0fb5762fe03e6c9697eebc99
zhaowumi <wuqiang@badambiz.com>
2019-01-03 22:04:00 +0800

    encry

--- huoliquankai/minigame_hlqk/src/common/User.ts
+++ huoliquankai/minigame_hlqk/src/common/User.ts
@@ -1,8 +1,1 @@
-class User {
-	public static openId = ""
-	public static roomId = ""
-	public static token = 0
-	public static sessionid = 0
-	public constructor() {
-	}
-}
+a3cd7fef9abb90330103fcc85b2cfe7e900aa09d02d712ef2a6bee4e28bd58556e93748855df46a0f4bd6389b65bbf7e5e0763a325c918356f64b11019399072707119b600d493010d4ff0071d529d0ddbaa81b59648fa97a80a5372049ed2aedabd49a74a5dea0b1740ed095e0d79438201b2c5203fe208dfb39b26e54776545cd5815d1d50200ed178067d59f07d98f9d452da35d1adf0fe3f17baf041a012
